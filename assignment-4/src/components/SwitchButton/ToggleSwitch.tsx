b12973a45b053b05fc0687b29e61af74b934f8d5
Tu Pham <tupx1204@gmail.com>
2023-10-07 00:40:21 +0700

    forgot to save this file

--- assignment-4/src/components/SwitchButton/ToggleSwitch.tsx
+++ assignment-4/src/components/SwitchButton/ToggleSwitch.tsx
@@ -3,14 +3,9 @@ import './ToggleSwitch.css'
 import { SwitchButtonType } from '../../utils/types'
 
 export const Switch = ({ isOn, handleToggle, label }: SwitchButtonType) => {
-  // const colorOne = '#324c5c'
-  // const colorTwo = '#70e2fe'
-  // const colorButOne = '#F5F3CE'
-  // const colorButTwo = '#f28c38'
   return (
     <div id="toggleWrapper">
       <label
-        // style={{ background: isOn ? colorOne : colorTwo }}
         className={`switch-label ${isOn ? `bg-[#324c5c]` : `bg-[#70e2fe]`}`}
         htmlFor="switch"
       >
@@ -23,7 +18,6 @@ export const Switch = ({ isOn, handleToggle, label }: SwitchButtonType) => {
         />
         <span
           className={`switch-button ${isOn ? `bg-[#F5F3CE]` : `bg-[#f28c38]`}`}
-          // style={{ background: isOn ? colorButOne : colorButTwo }}
         />
       </label>
       <p className="text-emerald-400">{label}</p>
