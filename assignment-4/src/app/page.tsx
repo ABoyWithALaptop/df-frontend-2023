d583c0e1d2e857dee93a603f15e8d34cc5b284ea
Tu Pham <tupx1204@gmail.com>
2023-10-07 15:11:54 +0700

    don't mind me, just deleted some commented code

--- assignment-4/src/app/page.tsx
+++ assignment-4/src/app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
           <PaginationBar data={books} />
         </section>
       </main>
-
       {isModalAddOpen && <AddBookModal setIsModalAddOpen={setIsModalAddOpen} />}
       {deleteItem && <DeleteBookModal />}
     </>
